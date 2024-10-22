c0f46b1f7ebc53cf7e4169b23e83fad14dc4787f
Danny Hintze <114960227+DJCodeRepo@users.noreply.github.com>
2024-10-21 19:47:29 -0700

    Add functions to get computer and user choices

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 console.log('Hello World');
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * (99));
+    if (num < 33) return "rock";
+    if (num < 66) return "paper";
+    return "scissors";
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let input = prompt("Rock, paper, or scissors? ");
+    return input.toLowerCase();
+}
+
+console.log(getHumanChoice());
